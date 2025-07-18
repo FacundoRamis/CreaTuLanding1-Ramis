a685b4a1a6593d72b3f879c8744d8ddf854f2e9f
FacundoRamis <facundoeoliver@gmail.com>
2025-07-18 00:52:28 -0300

    Add files via upload

---
+++ Segunda entrega/src/pages/Contacto.jsx
@@ -0,0 +1,10 @@
+const Contacto = () => {
+  return (
+    <div>
+      <h2>Contacto</h2>
+      <p>¡Próximamente podrás contactarnos desde acá!</p>
+    </div>
+  );
+};
+
+export default Contacto;
